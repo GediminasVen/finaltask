f319613bc3748710e0cbeefe506d975256c2b8dc
gediminas <venslovaitisgediminas@gmail.com>
2023-11-13 18:36:58 +0200

    Add Registration test cases

--- src/test/java/lt/gediminas/finalexam/pages/zalando/RegistrationPage.java
+++ src/test/java/lt/gediminas/finalexam/pages/zalando/RegistrationPage.java
@@ -2,16 +2,38 @@ package lt.gediminas.finalexam.pages.zalando;
 
 import lt.gediminas.finalexam.pages.Common;
 import lt.gediminas.finalexam.pages.Locator;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
-public class RegistrationPage {
+public class RegistrationPage extends Common{
     public static void openChrome(String url) {
         Common.setUpChrome();
         Common.openUrl(url);
     }
-    public static void clickOnRegister(){
+    public static void clickOnRegister() {
         Common.clickOnElement(Locator.Zalando.Registration.ClickOnRegistration);
         Common.clickOnElement(Locator.Zalando.Registration.ClickOnRegistrationWhenVisible);
+    }
+
+    public static void enterName(String name) {
+        Common.sendKeysToElement(Locator.Zalando.Registration.enterName, name);
+    }
+
+    public static void enterLastname(String lastname) {
+        Common.sendKeysToElement(Locator.Zalando.Registration.enterLastname, lastname);
+    }
+
+    public static void enterEmail(String email) {
+        Common.sendKeysToElement(Locator.Zalando.Registration.enterEmail, email);
+    }
+
+    public static void enterPassword(String password) {
+        Common.sendKeysToElement(Locator.Zalando.Registration.enterPassword, password);
+    }
+
+    public static String isRegistrationSuccessful() {
+        return Common.getTextFromElement(Locator.Zalando.Registration.readSuccesfulRegistration);
+    }
+
+    public static void clickOnRegisterCheckProvidedData() {
+        Common.clickOnElementWhenAvailableCustomised(Locator.Zalando.Registration.clickOnRegistrationAfterDataProvided, 5);
     }
 }
--- src/test/java/lt/gediminas/finalexam/tests/zalando/RegistrationTest.java
+++ src/test/java/lt/gediminas/finalexam/tests/zalando/RegistrationTest.java
@@ -2,8 +2,13 @@ package lt.gediminas.finalexam.tests.zalando;
 
 import lt.gediminas.finalexam.pages.zalando.RegistrationPage;
 import lt.gediminas.finalexam.tests.TestBase;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 
 public class RegistrationTest extends TestBase {
     @BeforeMethod
@@ -11,9 +16,36 @@ public class RegistrationTest extends TestBase {
     public void setUp() {
         RegistrationPage.openChrome("https://www.zalando.lt/");
     }
+
     @Test
     public void testClickOnRegistration() {
         RegistrationPage.clickOnRegister();
     }
 
+    @DataProvider(name = "registrationFields")
+    public Object[][] provideDataForRegistration() {
+        return new Object[][]{
+                {"Gediminas", "Venslovaitis", "venslovaitisgediminas@yahoo.lt", "Abece2le1$2s$3Spsswtr3!"},
+                {"Gediminas", "Venslovaitis", "venslovaitisgediminas@one.lt", " "},
+                {"Gediminas", "Venslovaitis", " @one.lt", " "},
+                {"Gediminas", "2[", " @one.lt", " "},
+                {" ", " ", " ", " "},
+
+
+        };
+    }
+
+    @Test(dataProvider = "registrationFields")
+    public void testDataForRegistration(String name, String lastname, String email, String password) {
+        RegistrationPage.clickOnRegister();
+        RegistrationPage.enterName(name);
+        RegistrationPage.enterLastname(lastname);
+        RegistrationPage.enterEmail(email);
+        RegistrationPage.enterPassword(password);
+        RegistrationPage.clickOnRegisterCheckProvidedData();
+        String actualResult;
+        actualResult = RegistrationPage.isRegistrationSuccessful();
+        Assert.assertTrue(RegistrationPage.isRegistrationSuccessful().contains("Stiprus")
+                , "Stiprus slaptažodis.");
+    }
 }
